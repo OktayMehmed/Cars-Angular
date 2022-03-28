cc5dd6bb7bcfbe34e79d402cea4b7a19559e395f
Oktay Mehmed <oktay888@abv.bg>
2022-03-28 15:19:22 +0300

    Modeling Data

---
+++ backend/models/Cars.js
@@ -0,0 +1,63 @@
+const mongoose = require("mongoose");
+const { ObjectId } = mongoose.SchemaTypes;
+
+const carsSchema = mongoose.Schema(
+  {
+    user: {
+      type: ObjectId,
+      required: true,
+      ref: "User",
+    },
+
+    make: {
+      type: String,
+      required: true,
+    },
+
+    model: {
+      type: String,
+      required: true,
+    },
+
+    image: {
+      type: String,
+      required: true,
+    },
+
+    price: {
+      type: Number,
+      required: true,
+    },
+
+    year: {
+      type: Number,
+      required: true,
+    },
+
+    fuel: {
+      type: String,
+      required: true,
+    },
+
+    color: {
+      type: String,
+      required: true,
+    },
+
+    power: {
+      type: Number,
+      required: true,
+    },
+
+    description: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true
+  }
+);
+
+const Cars = mongoose.model("Cars", carsSchema);
+module.exports = Cars;
