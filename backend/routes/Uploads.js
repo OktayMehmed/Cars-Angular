3a7930e9e78f5e727765d342580d805ac4c73e28
Oktay Mehmed <oktay888@abv.bg>
2022-04-15 16:24:14 +0300

    Image Upload Config

---
+++ backend/routes/Uploads.js
@@ -0,0 +1,38 @@
+const path = require("path");
+const express = require("express");
+const multer = require("multer");
+const router = express.Router()
+
+const storage = multer.diskStorage({
+  destination(req, file, cb) {
+    cb(null, 'uploads/')
+  },
+  filename(req, file, cb) {
+    cb(null, `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`)
+  }
+})
+
+function checkFileType(req, file, cb) {
+  const fileType = /jpg|jpeg|png/
+  const extname = fileType.test(path.extname(file.originalname).toLowerCase())
+  const mimetype = fileType.test(file.mimetype)
+
+  if (extname && mimetype) {
+    return cb(null, true)
+  } else {
+    cb('Images only!')
+  }
+}
+
+const upload = multer({
+  storage,
+  fileFilter: function (req, file, cb) {
+    checkFileType(file, cb)
+  }
+})
+
+router.post('/', upload.single('image'), (req, res) => {
+  res.send(`/${req.file.path}`)
+})
+
+module.exports = router
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const path = require("path");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const connectDB = require('./config/db');
 const Cars = require('./routes/Cars');
 const User = require('./routes/User');
+const Upload = require('./routes/Uploads');
 
 dotenv.config();
 
@@ -23,6 +25,10 @@ app.get("/", (req, res) => {
 
 app.use('/api/cars', Cars);
 app.use('/api/users', User);
+app.use('/api/upload', Upload);
+
+__dirname = path.resolve();
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
 const PORT = process.env.PORT || 8000;
 
